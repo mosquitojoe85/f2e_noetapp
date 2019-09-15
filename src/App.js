2ddff9293e9feac21197076eb384ee30a1f9c097
Unknown <mosquitojoe85@gmail.com>
2019-09-16 00:31:27 +0800

    [ADD] 增加筆記列表

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Icon from '@mdi/react'
+import { mdiMenu, mdiFormatListBulleted, mdiApps } from '@mdi/js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appWrapper">
+      <div className="app">
+        <header>
+          <button><Icon path={mdiMenu} size={'20px'} horizontal /></button>
+          <div className="rightBlock">
+            <button><Icon path={mdiApps} size={'20px'} horizontal /></button>
+            <button><Icon path={mdiFormatListBulleted} size={'20px'} horizontal /></button>
+          </div>
+        </header>
+        <main>
+          <h1>所有筆記</h1>
+          <p>共有 10 個筆記</p>
+          <button className="newNoteBtn">New Note</button>
+          <div className="noteItem">
+            <h2>靈感隨手記</h2>
+            <p>靈感總是說來就來，所以就隨手記一下～</p>
+          </div>
+          <div className="noteItem">
+            <h2>無標題</h2>
+            <p>業們是上即善時心現背藥此望角西少，我以況這，心在如對今。次後其本電很我要檢期告創就張點禮文市腦什...</p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
